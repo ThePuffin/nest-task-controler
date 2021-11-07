c2f8fd9ff57eca5bfea12569e1dbc100548e077b
Vinz <ballutvincent@gmail.com>
2021-11-07 15:35:02 +0100

    create error if not found

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 
 import { CreateTaskDto } from './create-task.dto';
@@ -15,7 +15,11 @@ export class TasksService {
   }
 
   public getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException();
+    }
+    return found;
   }
 
   public getTaskskWithFilters(filterDto: GetTasksFilterDto): Task[] {
