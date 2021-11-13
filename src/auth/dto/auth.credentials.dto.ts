aecbc994390b238caf6f51d299bd12676d23e347
Vinz <ballutvincent@gmail.com>
2021-11-13 18:02:24 +0100

    check password and username

--- src/auth/dto/auth.credentials.dto.ts
+++ src/auth/dto/auth.credentials.dto.ts
@@ -1,4 +1,19 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+  @IsString()
+  @MinLength(8, {
+    message: 'password is too short',
+  })
+  @MaxLength(20, {
+    message: 'password is too long',
+  })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password is weak',
+  })
   password: string;
 }
