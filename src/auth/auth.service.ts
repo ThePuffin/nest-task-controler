713c235cd076104f177ea802bf20fcd4ff9b25a6
Vinz <ballutvincent@gmail.com>
2021-11-13 17:57:17 +0100

    create endpoint for user creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
+import { AuthCredentialsDto } from './dto/auth.credentials.dto';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
@@ -9,4 +10,8 @@ export class AuthService {
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    this.usersRepository.createUser(authCredentialsDto);
+  }
 }
